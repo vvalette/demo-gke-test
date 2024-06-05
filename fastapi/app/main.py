87dcaafbb2559ed3a6855d3411202643c4aa1bfa
Valentin VALETTE <valentin.valette@orange.com>
2024-06-05 16:25:00 +0200

    release

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -18,7 +18,7 @@ async def get_users_from_db():
         user='test-demo',
         password='test-demo',
         database='my_database',
-        host='34.163.192.144',
+        host='127.0.0.1',
         port=5432
     )
     users = await conn.fetch("SELECT * FROM users")  # Adjust your query as needed
