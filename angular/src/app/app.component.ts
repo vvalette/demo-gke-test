3200ec1899e38af2fae735326e034d8c78cf4cab
Valentin VALETTE <valentin.valette@orange.com>
2024-06-11 09:06:42 +0200

    release

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BackendService } from './backend';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +10,21 @@ import { BackendService } from './backend';
 })
 export class AppComponent implements OnInit {
   users: any[] = [];
+  commitDate: string | null = null;
 
-  constructor(private backendService: BackendService) { }
+  constructor(private backendService: BackendService, private http: HttpClient) {}
+
+  getCommitDate(): Observable<string> {
+    return this.http.get('assets/commit-date.txt', { responseType: 'text' });
+  }
 
   ngOnInit(): void {
     this.backendService.getUsers().subscribe(users => {
       this.users = users;
+    });
+
+    this.getCommitDate().subscribe(date => {
+      this.commitDate = date;
     });
   }
 }
