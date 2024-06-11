27b74bf43383d930e28cfb1e06b58ed22baee427
Valentin VALETTE <valentin.valette@orange.com>
2024-06-11 15:15:44 +0200

    release

--- angular/entrypoint.sh
+++ angular/entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 echo "BACKEND_IP=$BACKEND_IP"
+echo "ENV=$ENV"
 current_date=$(TZ="Europe/Paris" date +'%Y-%m-%d %H:%M:%S')
 
 # Replace backend IP in the environment.prod.ts file
 sed -i "s|<backend-service-ip>|$BACKEND_IP|g" /usr/share/nginx/html/main*.js
 sed -i "s|<build-date>|$current_date|g" /usr/share/nginx/html/main*.js
+sed -i "s|{env}|$ENV|g" /usr/share/nginx/html/main*.js
 
 # Start Nginx
 nginx -g 'daemon off;'
